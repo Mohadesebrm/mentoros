ad62ef5d09ccc1a9bf2fde5799aae0bf0eb3f538
Mohi <mohi@Mohis-MacBook-Air.local>
2026-08-09 14:30:08 +0330

    Build student list and status badges

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,5 +1,42 @@
 export default function Students () {
+    const students=[
+    {
+       id:1,
+        name: "Mohadese",
+        email:"Mohadese@example.com",
+        status:"Active"
+    },
+    {
+        id:2,
+        name:"Sam",
+        email:"samsamaii@gmail.com",
+        status:"Inactive"
+    },
+    {
+        id:3,
+        name:"sara",
+        email:"Sara@gmail.com",
+        status:"Active"
+    },
+    ];
     return (
+        <div className="flex flex-col gap-4">
         <h1>Students Page</h1>
+        {students.map((student)=>(
+            <div key={student.id} className="border p-4 rounded">
+              <p className="font-bold">{student.name}</p>
+              <p className="text-sm">{student.email}</p>
+              <p
+              className={
+                student.status==="Active"
+                ?"inline-block bg-green-100 text-green-600 font-bold px-2 py-1 rounded"
+                :"inline-block bg-gray-200 text-gray-500 text-sm px-2 py-1 rounded"
+              }
+              >
+                {student.status}
+                </p>
+                </div>
+        ))}
+        </div>
     );
 }
